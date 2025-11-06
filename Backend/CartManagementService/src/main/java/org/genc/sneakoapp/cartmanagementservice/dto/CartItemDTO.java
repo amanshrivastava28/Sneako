94d2bdcc6c6121a12a212a24a15913fc8cc2b877
Shivam Chopra CTS <Shivam.Chopra2@cognizant.com>
2025-11-06 14:16:38 +0530

    cart-management-service

---
+++ Backend/CartManagementService/src/main/java/org/genc/sneakoapp/cartmanagementservice/dto/CartItemDTO.java
@@ -0,0 +1,24 @@
+package org.genc.sneakoapp.cartmanagementservice.dto;
+
+import jakarta.annotation.Nullable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CartItemDTO {
+    @Nullable
+    private Long cartItemId;
+    private Long userId;
+    private Long productId;
+    private double unitPrice;
+    private Long quantity;
+
+    @Nullable
+    private double totalPrice;
+    private Integer size; // Added size field
+}
